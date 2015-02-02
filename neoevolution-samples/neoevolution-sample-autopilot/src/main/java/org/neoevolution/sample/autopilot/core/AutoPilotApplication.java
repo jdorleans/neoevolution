cd3347b2ea734953d8cb2a68411f2f79e7f6e92a
jdorleans <jonathan.dorleans@gmail.com>
2015-02-02 09:30:05 -0300

    AutoPilotNeoEvolution - Fix jump keys

--- neoevolution-samples/neoevolution-sample-autopilot/src/main/java/org/neoevolution/sample/autopilot/core/AutoPilotApplication.java
+++ neoevolution-samples/neoevolution-sample-autopilot/src/main/java/org/neoevolution/sample/autopilot/core/AutoPilotApplication.java
@@ -496,9 +496,9 @@ public class AutoPilotApplication extends ApplicationAdapter {
                 speed += increase(speed, 5);
                 updatePhysics();
             }
-            else if (keycode == Input.Keys.UP) {
+            else if (keycode == Input.Keys.DOWN) {
                 jump -= decrease(jump, 0);
-            } else if (keycode == Input.Keys.DOWN) {
+            } else if (keycode == Input.Keys.UP) {
                 jump += increase(jump, 5);
             }
             return true;
