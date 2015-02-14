f9863ea9c3c5961dbec5a33645cc9a57ec71e59b
jdorleans <jonathan.dorleans@gmail.com>
2015-02-14 12:40:14 -0300

    NeoEvolution - Added Neo4j configuration properties

--- neoevolution/src/main/java/org/neoevolution/configuration/NENeo4jConfiguration.java
+++ neoevolution/src/main/java/org/neoevolution/configuration/NENeo4jConfiguration.java
@@ -1,6 +1,7 @@
 package org.neoevolution.configuration;
 
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.factory.GraphDatabaseBuilder;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.neoevolution.mvc.converter.ActivationToStringConverter;
 import org.neoevolution.mvc.converter.StringToActivationConverter;
@@ -11,12 +12,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.core.convert.converter.ConverterRegistry;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.neo4j.config.EnableNeo4jRepositories;
 import org.springframework.data.neo4j.config.Neo4jConfiguration;
 import org.springframework.data.neo4j.rest.SpringRestGraphDatabase;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -48,7 +52,7 @@ public class NENeo4jConfiguration extends Neo4jConfiguration {
 
 
     @PostConstruct
-    private void init() {
+    private void init() throws IOException {
         configurePackages();
         configureDatabase();
     }
@@ -58,12 +62,19 @@ public class NENeo4jConfiguration extends Neo4jConfiguration {
         setBasePackage(packages.toArray(new String[packages.size()]));
     }
 
-    private void configureDatabase()
+    private void configureDatabase() throws IOException
     {
         if (isRest) {
             setGraphDatabaseService(new SpringRestGraphDatabase(address));
-        } else {
-            setGraphDatabaseService(new GraphDatabaseFactory().newEmbeddedDatabase(address));
+        }
+        else {
+            File file = new ClassPathResource("neo4j.properties").getFile();
+            GraphDatabaseBuilder dbBuilder = new GraphDatabaseFactory().newEmbeddedDatabaseBuilder(address);
+
+            if (file.exists()) {
+                dbBuilder = dbBuilder.loadPropertiesFromFile(file.getAbsolutePath());
+            }
+            setGraphDatabaseService(dbBuilder.newGraphDatabase());
         }
     }
 
